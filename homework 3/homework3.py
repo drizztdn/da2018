f749c65639660f5a2594465668fba1f2250e986b
Kevin C. Athey <athey1@llnl.gov>
2018-04-06 11:12:07 -0700

    cleaned up to have a function to do predictions, for use in part b

--- homework 3/homework3.py
+++ homework 3/homework3.py
@@ -7,51 +7,36 @@ import seaborn as sns;
 
 pd.options.mode.chained_assignment = None
 
-def fx(x):
-    if x:
-        return 'Down'
-    else:
-        return 'Up'
+def predict(trn, smpl):
+    res = sm.glm(formula="Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5",data=trn,family=sma.families.Binomial()).fit()
+    print(res.summary())
 
-df=pd.read_csv("smarket.csv")
-trn = df.copy()
+    ypnew = res.predict(smpl)
+    smpl['pred'] = ypnew
+    smpl['pred_dir'] = ''
+    smpl['pred_correct'] = False
+    smpl['pred_dir'][smpl['pred'] > .5] = "Down"
+    smpl['pred_dir'][smpl['pred'] <= .5] = "Up"
+    smpl['pred_correct'] = smpl['pred_dir'] == smpl['Direction']
+    print(smpl.groupby('pred_correct')['pred'].count())
+    return smpl
 
-print(df.describe())
 
+if __name__ == "__main__":
+    df=pd.read_csv("smarket.csv")
+    trn = df.copy()
 
-sns.set(style="ticks", color_codes=True)
-g = sns.pairplot(df)
-g.savefig('pairwise.png')
+    print(df.describe())
 
-print(df.corr())
+    sns.set(style="ticks", color_codes=True)
+    g = sns.pairplot(df)
+    g.savefig('pairwise.png')
 
-res = sm.glm(formula="Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5",data=df,family=sma.families.Binomial()).fit()
-print(res.summary())
+    print(df.corr())
 
-ypnew = res.predict(df)
-df['pred'] = ypnew
-df['pred_dir'] = ''
-df['pred_correct'] = False
-df['pred_dir'][df['pred'] > .5] = "Down"
-df['pred_dir'][df['pred'] <= .5] = "Up"
-df['pred_correct'] = df['pred_dir'] == df['Direction']
-#print(df)
+    df = predict(df, df)
 
-print(df.groupby('pred_correct')['pred'].count())
+    smpl = trn[trn["Year"] == 2005]
+    trn = trn.drop(smpl.index)
 
-smpl = trn[trn["Year"] == 2005]
-trn = trn.drop(smpl.index)
-
-res2 = sm.glm(formula="Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5",data=trn,family=sma.families.Binomial()).fit()
-yp2new = res2.predict(smpl)
-
-smpl['pred'] = yp2new
-smpl['pred_dir'] = ''
-smpl['pred_correct'] = False
-smpl['pred_dir'][smpl['pred'] > .5] = "Down"
-smpl['pred_dir'][smpl['pred'] <= .5] = "Up"
-smpl['pred_correct'] = smpl['pred_dir'] == smpl['Direction']
-
-print(smpl.groupby('pred_correct')['pred'].count())
-
-#print(yp2new)
+    smpl = predict(trn, smpl)
