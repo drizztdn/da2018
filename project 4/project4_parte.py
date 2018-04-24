b603e389c5aac5ad65a7636a333d0711f418f5ce
Kevin C. Athey <athey1@llnl.gov>
2018-04-24 08:25:50 -0700

    formatting fix

--- project 4/project4_parte.py
+++ project 4/project4_parte.py
@@ -10,40 +10,40 @@ d['ReleaseYear'] = d['ReleaseYear'].astype('str')
 formula = "ReservesLevel ~ Platform*Region*ReleaseMonth*Channel*Edition*RelativeWeek"
 output = "ReservesLevel"
 
-model = sma.load('best_model.pickle')
+# model = sma.load('best_model.pickle')
 
 def loocv(d, formula, output):
     loo = cross_val.LeaveOneOut(len(d.index))
     error_sum = 0
 
     for train_index, test_index in loo:
-             a_train, a_test = cross_val.split(train_index,test_index,d)
-             d_train = pd.DataFrame(a_train,columns=d.columns)
-             d_test = pd.DataFrame(a_test,columns=d.columns)
-             for x in d.columns:
-                 d_train[x] = d_train[x].astype(d[x].dtypes.name)
-                 d_test[x] = d_test[x].astype(d[x].dtypes.name)
-             nuc = sm.ols(formula, data=d_train).fit()
-             y = nuc.predict(d_test)
-             error_sum+= (y[0] - d_test[output][0])**2
-
+        print("TRAIN:", train_index, "TEST:", test_index)
+        a_train, a_test = cross_val.split(train_index,test_index,d)
+        d_train = pd.DataFrame(a_train,columns=d.columns)
+        d_test = pd.DataFrame(a_test,columns=d.columns)
+        for x in d.columns:
+            d_train[x] = d_train[x].astype(d[x].dtypes.name)
+            d_test[x] = d_test[x].astype(d[x].dtypes.name)
+        nuc = sm.ols(formula, data=d_train).fit()
+        y = nuc.predict(d_test)
+        error_sum+= (y[0] - d_test[output][0])**2
     print("LOOCV MSE= ", (error_sum/len(d.index)))
+
 
 def kFold(d, formula, output, size):
     loo = cross_val.KFold(len(d.index),size)
     error_sum = 0
     for train_index, test_index in loo:
-            # print ("TRAIN:", train_index, "TEST:", test_index)
-            a_train, a_test = cross_val.split(train_index,test_index,d)
-            d_train = pd.DataFrame(a_train,columns=d.columns)
-            d_test = pd.DataFrame(a_test,columns=d.columns)
-            for x in d.columns:
-                d_train[x] = d_train[x].astype(d[x].dtypes.name)
-                d_test[x] = d_test[x].astype(d[x].dtypes.name)
-            nuc = sm.ols(formula, data=d_train).fit()
-            y = nuc.predict(d_test)
-            error_sum+= ((y - d_test[output])**2).sum()/len(d_test.index)
-
+        print ("TRAIN:", train_index, "TEST:", test_index)
+        a_train, a_test = cross_val.split(train_index,test_index,d)
+        d_train = pd.DataFrame(a_train,columns=d.columns)
+        d_test = pd.DataFrame(a_test,columns=d.columns)
+        for x in d.columns:
+            d_train[x] = d_train[x].astype(d[x].dtypes.name)
+            d_test[x] = d_test[x].astype(d[x].dtypes.name)
+        nuc = sm.ols(formula, data=d_train).fit()
+        y = nuc.predict(d_test)
+        error_sum+= ((y - d_test[output])**2).sum()/len(d_test.index)
     print("k-Fold " + size +" MSE= ", (error_sum/size))
 
 loocv(d, formula, output)
