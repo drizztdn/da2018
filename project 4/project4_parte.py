753a08b1c77c1328c79ac27977c128825bd4f1f6
Kevin C. Athey <athey1@llnl.gov>
2018-04-30 20:30:18 -0700

    updated for cleaned model and logging

--- project 4/project4_parte.py
+++ project 4/project4_parte.py
@@ -5,18 +5,21 @@ import statsmodels.api as sma
 import statsmodels.sandbox.tools.cross_val as cross_val
 import datetime
 
-print(datetime.datetime.now())
+file = open('validation.txt','w')
+
+print(datetime.datetime.now(), file=file)
 d = pd.read_csv("cleaned.csv")
 d = d.reindex(np.random.permutation(d.index))
 d['ReleaseMonth'] = d['ReleaseMonth'].astype('str')
 d['ReleaseYear'] = d['ReleaseYear'].astype('str')
 
-formula = "ReservesLevel ~ Platform*ReleaseMonth"
+formula = "ReservesLevel ~ ReleaseYear*Platform*ReleaseMonth*Edition*RelativeWeek"
 output = "ReservesLevel"
 
 # model = sma.load('best_model.pickle')
 
 def loocv(d, formula, output):
+    print('processing loocv', file=file)
     print('processing loocv')
     loo = cross_val.LeaveOneOut(len(d.index))
     error_sum = 0
@@ -31,10 +34,12 @@ def loocv(d, formula, output):
         nuc = sm.ols(formula, data=d_train).fit()
         y = nuc.predict(d_test)
         error_sum += (y[0] - d_test['ReservesLevel'][0]) ** 2
+    print("loocv MSE= ", (error_sum / len(d.index)), file=file)
     print("loocv MSE= ", (error_sum / len(d.index)))
 
 
 def kFold(d, formula, output, size):
+    print('processing kFold: ' + str(size), file=file)
     print('processing kFold: ' + str(size))
     loo = cross_val.KFold(len(d.index), size)
     error_sum = 0
@@ -46,14 +51,20 @@ def kFold(d, formula, output, size):
         for x in d.columns:
             d_train[x] = d_train[x].astype(d[x].dtypes.name)
             d_test[x] = d_test[x].astype(d[x].dtypes.name)
-        nuc = sm.ols('ReservesLevel ~ Platform*ReleaseMonth', data=d_train).fit()
+        nuc = sm.ols(formula, data=d_train).fit()
         y = nuc.predict(d_test)
         error_sum += ((y - d_test['ReservesLevel']) ** 2).sum() / len(d_test.index)
-    print("k-Fold {} MSE= {}".format(size,(error_sum / size)))
+    print("k-Fold {} MSE= {}".format(size,(error_sum / size)), file=file)
+    print("k-Fold {} MSE= {}".format(size, (error_sum / size)))
 
-loocv(d, formula, output)
-print(datetime.datetime.now())
+print("validating : {}".format(formula), file=file)
+print("validating : {}".format(formula))
+# loocv(d, formula, output)
+# print(datetime.datetime.now(), file=file)
+# print(datetime.datetime.now())
 kFold(d, formula, output, 5)
+print(datetime.datetime.now(), file=file)
 print(datetime.datetime.now())
 kFold(d, formula, output, 10)
+print(datetime.datetime.now(), file=file)
 print(datetime.datetime.now())
